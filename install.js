2ab430637d070d78e6bf3f3f187ffbefcddecc0d
mickish <mickish@ndr.com>
2018-01-15 03:28:26 -0500

    Bot Mention Demo

--- install.js
+++ install.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const qs = require('querystring');
+const AWS = require('aws-sdk');
 const fetch = require('node-fetch');
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 const getCode = (event) => {
 	var code = null;
@@ -23,17 +26,26 @@ const requestToken = (code) => {
 	const url = `https://slack.com/api/oauth.access?${qs.stringify(params)}`;
 	console.log(`Fetching ${url}`);
 	return fetch(url)
-		.then(response => response.json()) // Convert response to JSOJN
+		.then(response => response.json()) // Convert response to JSON
 		.then((json) => {
 			if (json.ok) return json; // Verify is valid JSON
 			throw new Error('SlackAPIError');
 		});
 };
 
+const saveResponse = (response) => {
+	const params = {
+		TableName: process.env.TEAMS_TABLE,
+		Item: response
+	};
+	console.log('Put', params);
+	return dynamodb.put(params).promise();
+};
+
 const successResponse = callback => callback(null, {
 	statusCode: 302,
 	headers: { Location: process.env.INSTALL_SUCCESS_URL }
-})
+});
 
 const errorResponse = (error, callback) => {
 	console.error(error);
@@ -47,6 +59,6 @@ module.exports.handler = (event, context, callback) =>
 	Promise.resolve(event)
 		.then(getCode)  // Get code from event
 		.then(requestToken)  // Exchange code for token
-		// .then(saveResponse) // Save token to DDB
+		.then(saveResponse)  // Save token to DDB
 		.then(() => successResponse(callback))
 		.catch(error => errorResponse(error, callback));
